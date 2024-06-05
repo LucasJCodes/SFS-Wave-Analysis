10e15e7e9b2ca069eac69f9c9e24bdde7a7326a9
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-05 15:30:02 -0500

    Initial commit

---
+++ ex_figures/time_step_sigw.py
@@ -0,0 +1,9 @@
+#!/us/bin/env python3
+
+# This program creates 7 plots of significant wave height from a gefs run initialized at 19181101, running for 30 days.  One plot will be put on the same figure for each day
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
