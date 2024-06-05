a8319e23961a5b32abfc8471e458928ed1a13b66
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-05 12:52:39 -0500

    Initial commit

---
+++ ex_figures/sig_wave.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#This program retrieves data from a the 19971101 IC, 30 day run of the SFS and graphs significant wave height 
+
+def main():
+
+
+if __name__ == "main()":
+    main()
