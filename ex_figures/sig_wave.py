92f373df966c8238462d6a5e8c3a326bbf8bd7a9
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-05 14:10:08 -0500

    Produces figure

--- ex_figures/sig_wave.py
+++ ex_figures/sig_wave.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
-#This program retrieves data from a the 19971101 IC, 30 day run of the SFS and graphs significant wave height 
+#This program retrieves data from a the 19971101 IC, 30 day run of the gefs and graph significant wave height 
+
+import xarray as xr
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 
 def main():
 
+    path = "/work2/noaa/marine/ljones/test01/t05/COMROOT/t05/gefs.19971101/00/mem000/products/wave/gridded/"
+    file = "gefswave.t00z.global.1p00.f006.grib2.nc"
+    
+    data_in = xr.load_dataset(path + file)
 
-if __name__ == "main()":
+    #subset to get significant wave height  
+    sig_wave = data_in["HTSGW_surface"]
+
+    fig, ax = plt.subplots(nrows = 1, ncols = 1, subplot_kw = {"projection": ccrs.PlateCarree()})
+
+    ax.contourf(sig_wave["time" == 0], projections = ccrs.PlateCarree())
+    ax.coastlines()
+    
+    plt.savefig("fig_sigwave.png")
+
+if __name__ == "__main__":
     main()
