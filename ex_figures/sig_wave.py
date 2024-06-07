009804e3a378314a9489406ec990ddd5e42f806b
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-07 15:23:00 -0500

    Improved plotting and cleaned up data indexing

--- ex_figures/sig_wave.py
+++ ex_figures/sig_wave.py
@@ -9,19 +9,19 @@ import matplotlib.pyplot as plt
 def main():
 
     path = "/work2/noaa/marine/ljones/test01/t05/COMROOT/t05/gefs.19971101/00/mem000/products/wave/gridded/"
-    file = "gefswave.t00z.global.1p00.f006.grib2.nc"
+    file = "gefswave.t00z.global.1p00.f048.grib2.nc"
     
     data_in = xr.load_dataset(path + file)
 
     #subset to get significant wave height  
     data2d = data_in.isel(time = 0)
-    sig_wave = data2d["HTSGW_surface"]
 
     #plot the data on a contour plot
-    fig, ax = plt.subplots(nrows = 1, ncols = 1)
+    fig, ax = plt.subplots(nrows = 1, ncols = 1, subplot_kw = {"projection": ccrs.PlateCarree()})
 
-    ax.contourf( sig_wave)
+    ax.contourf(data2d["longitude"], data2d["latitude"], data2d["HTSGW_surface"], transform = ccrs.PlateCarree())
     ax.set_title("Significant Wave Height 1997-11-01")
+    ax.coastlines()
     
     plt.savefig("fig_sigwave.png")
 
