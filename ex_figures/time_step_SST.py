9868970b7a13b80db031b7d24f7b37bdc3e7582f
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-07 14:12:21 -0500

    Plotting data, but does not include coastlines correctly

--- ex_figures/time_step_SST.py
+++ ex_figures/time_step_SST.py
@@ -8,13 +8,16 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
 def main():
-
+    
+    # constants holding the arbitrary number of rows and columns in the figure
+    ROWS = 4
+    COLS = 2
     # read in all ocean files from the model run 
     path = "/work2/noaa/marine/ljones/test01/t05/COMROOT/t05/gefs.19971101/00/mem000/products/ocean/grib2/1p00/"
     files = glob.glob(path + "/*.nc")
 
     # prepare figure, axes for plotting
-    fig, ax = plt.subplots(4, 2, subplot_kw = {"projection": ccrs.PlateCarree()})
+    fig, ax = plt.subplots(ROWS, COLS, subplot_kw = {"projection": ccrs.Robinson()})
 
     # perform seven iterations, loading in data forward in time and then plotting
     for file in files:
@@ -22,8 +25,26 @@ def main():
         # read data in and subset
         data_in = xr.load_dataset(file)
         data2d = data_in.isel(time = 0)
-        sigwave = data2d["WTMP_surface"]
-        print(sigwave)
+        SST = data2d["WTMP_surface"]
+        #print(SST)
+        
+        # plot each file on its own ax
+
+        # creates i, j variables to determine the row and column to plot in
+        curr_i = files.index(file)
+
+        if curr_i > ROWS - 1:
+            i = curr_i - ROWS
+            j = 1
+
+        else:
+            i = curr_i
+            j = 0
+
+        test = ax[i][j].contourf(SST, transform = ccrs.Robinson())
+        ax[i][j].coastlines()
+
+    plt.savefig("SST_time_step.png")
         
 
 if __name__ == "__main__":
