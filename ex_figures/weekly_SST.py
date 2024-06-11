b17849c67a7b6598b164eda972ad2b8d173967a9
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-06-11 15:57:52 -0500

    Completed plots but improving slightly

--- ex_figures/weekly_SST.py
+++ ex_figures/weekly_SST.py
@@ -22,8 +22,8 @@ def main():
     nowave_in = xr.open_mfdataset(path_nowave)
     
     #subset
-    waveSST = wave_in["SST"]
-    nowaveSST = nowave_in["SST"]
+    waveSST = wave_in["SST"] - 273.15
+    nowaveSST = nowave_in["SST"] - 273.15
 
     #use groupby() to break into weekly chunks.  Note: warning about deprication of time.week expected here, possible other fix: https://github.com/pydata/xarray/discussions/6375
     weekly_wave = waveSST.convert_calendar(calendar = "standard").groupby("time.week").mean()
@@ -35,10 +35,34 @@ def main():
     week2 = diff_weekly[1]
     week3 = diff_weekly[2]
     week4 = diff_weekly[3]
+    week5 = diff_weekly[4]
 
-    fig, ax = plt.subplots(nrows = 2, ncols = 2, subplot_kw = {"projection": ccrs.PlateCarree()})
+    fig, ax = plt.subplots(nrows = 2, ncols = 3, subplot_kw = {"projection": ccrs.PlateCarree()})
 
-    ax[0][0].contourf(week1.longtitude, week1.latitude, week1, transform = ccrs.PlateCarree())
+    ax1 = ax[0][0].contourf(week1.xh, week1.yh, week1, transform = ccrs.PlateCarree(), cmap = "seismic")
+    ax[0][0].coastlines()
+    ax[0][0].set_title("Week 1")
+    fig.colorbar(ax1, location = "bottom", extend = "both", label = "deg C")
+
+    ax2 = ax[0][1].contourf(week2.xh, week2.yh, week2, transform = ccrs.PlateCarree(), cmap = "seismic")
+    ax[0][1].coastlines()
+    ax[0][1].set_title("Week 2")
+    fig.colorbar(ax2, location = "bottom", extend = 'both', label = "deg C")
+
+    ax3 = ax[0][2].contourf(week3.xh, week3.yh, week3, transform = ccrs.PlateCarree(), cmap = "seismic")
+    ax[0][2].coastlines()
+    ax[0][2].set_title("Week 3")
+    fig.colorbar(ax3, location = "bottom", extend = "both", label = "deg C")
+
+    ax4 = ax[1][0].contourf(week4.xh, week4.yh, week4, transform = ccrs.PlateCarree(), cmap = "seismic")
+    ax[1][0].coastlines()
+    ax[1][0].set_title("Week 4")
+    fig.colorbar(ax4, location = 'bottom', extend = "both", label = "deg C")
+
+    ax5 = ax[1][1].contourf(week5.xh, week5.yh, week5, transform = ccrs.PlateCarree(), cmap = "seismic")
+    ax[1][1].coastlines()
+    ax[1][1].set_title("Week 5")
+    fig.colorbar(ax5, location = "bottom", extend = "both", label = "deg C")
 
     plt.savefig("Weekly_SST.png")
     
