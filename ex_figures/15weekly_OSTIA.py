752dd01d410e58bd2f8661fe27be5f58611eea44
Lucas Jones <ljones@hercules-login-2.hpc.msstate.edu>
2024-06-12 08:07:31 -0500

    Initial commit and read in data

---
+++ ex_figures/15weekly_OSTIA.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+############################
+#Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
+#Date: 6/11/24
+############################
+
+import xarray as xr
+import datetime as dt
+
+def main():
+
+    ostia_path = "/work2/noaa/marine/jmeixner/ReferenceData/sst_OSTIA/0p05/2015/201511**-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc" 
+    wave_path = "/work2/noaa/marine/ljones/30day_tests/Nov15_S2S/COMROOT/Nov15_S2S/gefs.20151101/00/mem000/products/ocean/netcdf/*.nc"
+    nowave_path = "/work2/noaa/marine/ljones/30day_tests/Nov15_S2SW/COMROOT/Nov15_S2SW/gefs.20151101/00/mem000/products/ocean/netcdf/*.nc"
+
+    #read the data in
+    ostia_in = xr.open_mfdataset(ostia_path)
+    wave_in = xr.open_mfdataset(wave_path)
+    nowave_in = xr.open_mfdataset(nowave_path)
+
+    #subset the model data with more that SST variables and convert to Celsius
+    ostia_ds = ostia_in["analysed_sst"] - 273.15 #deg c
+    wave_ds = wave_in["SST"] - 273.15 #deg C
+    nowave_ds = nowave_in["SST"] - 273.15 #deg C
+
+    #use groupby() to put data into weekly segments 
+    ostia_ds = ostia_in.groupby("time.week")
+    ostia_ds2 = ostia_in.convert_calendar(calendar = "standard").groupby("time.week")
+
+    print(ostia_ds[45].time)
+    print(ostia_ds2[45].time)
+
+if __name__ == "__main__":
+    main()
