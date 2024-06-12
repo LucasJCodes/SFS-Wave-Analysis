7b2c0cbda0b90a6e99deca5d1f79e60e68f0ca42
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-12 16:11:09 -0500

    Initial commit

---
+++ ex_figures/850height_20.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+###############################
+#Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
+#Date: 6/12/24
+###############################
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
