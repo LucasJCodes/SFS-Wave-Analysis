92e7c7b344fd7b128b9c7171ff21b86b68e87230
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-14 08:40:22 -0500

    Read in wave model output

--- ex_figures/850height_20.py
+++ ex_figures/850height_20.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 #SBATCH -J runpy
+#SBATCH -o outfile
+#SBATCH -e errorfile
 #SBATCH -A marine-cpu
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
@@ -14,19 +16,38 @@
 ###############################
 
 import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 def main():
 
-    path = "/work2/noaa/marine/ljones/30day_tests/Nov20_S2S/COMROOT/Nov20_S2S/gefs.20201101/00/mem000/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f***.nc"
+    path_nowave = "/work2/noaa/marine/ljones/30day_tests/Nov20_S2S/COMROOT/Nov20_S2S/gefs.20201101/00/mem000/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f***.nc"
+    path_wave = "/work2/noaa/marine/ljones/30day_tests/Nov20_S2SW/COMROOT/Nov20_S2SW/gefs.20201101/00/mem000/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f***.nc"
 
-    ds = xr.open_mfdataset(path)
+    ds_nowave = xr.open_mfdataset(path_nowave)
+    ds_wave = xr.open_mfdataset(path_wave)
 
     #subset data to get 850 hPa heights only
-    data_850 = ds["HGT_850"]
-    print(data_850)
+    nowave_850 = ds_nowave["HGT_850mb"]
+    wave_850 = ds_wave["HGT_850mb"]
 
     #slice into 7 day periods (14 days after initialization and 14 days leading up to model run end
+    now_week1 = nowave_850.sel(time = slice("2020-11-01", "2020-11-07"))
+    now_week2 = nowave_850.sel(time = slice("2020-11-8", "2020-11-14"))
+    now_week3 = nowave_850.sel(time = slice("2020-11-18", "2020-11-24"))
+    now_week4 = nowave_850.sel(time = slice("2020-11-25", "2020-12-01"))
+    
+    w_week1 = wave_850.sel(time = slice("2020-11-01", "2020-11-07"))
 
+    #calculate differnces for plotting
+    diff1 = w_week1 - now_week1
+
+    #plot the differences
+    fig, axs = plt.subplots(nrows = 2, ncols = 2, subplot_kw = {"projection": ccrs.PlateCarree()})
+
+    ax1 = axs[0][0].contourf(diff1.xh, diff1.yh, diff1, transform = ccrs.PlateCarree())
+
+    plt.savefig("pres850_comp.png")
 
 if __name__ == "__main__":
     main()
