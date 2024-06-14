d3d57a7f37436d010fac6e93f5d947242bdcdceb
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-14 13:03:28 -0500

    Aligned OSTIA data to match model output

--- ex_figures/15weekly_OSTIA.py
+++ ex_figures/15weekly_OSTIA.py
@@ -23,12 +23,12 @@ import cartopy.crs as ccrs
 
 def main():
 
-    ostia_path = "/work2/noaa/marine/jmeixner/ReferenceData/sst_OSTIA/1p00/daily/sst_OSTIA.2015******.1p00.nc" 
+    ostia_path = "/work2/noaa/marine/jmeixner/ReferenceData/sst_OSTIA/1p00/daily/sst_OSTIA.20151*****.1p00.nc" 
     wave_path = "/work2/noaa/marine/ljones/30day_tests/Nov15_S2S/COMROOT/Nov15_S2S/gefs.20151101/00/mem000/products/ocean/netcdf/*.nc"
     nowave_path = "/work2/noaa/marine/ljones/30day_tests/Nov15_S2SW/COMROOT/Nov15_S2SW/gefs.20151101/00/mem000/products/ocean/netcdf/*.nc"
 
     #read the data in
-    ostia_in = xr.open_mfdataset(ostia_path) #.rename_dims({"lat": "xh", "lon": "yh"})
+    ostia_in = xr.open_mfdataset(ostia_path).rename({"lat": "yh", "lon": "xh"})
     wave_in = xr.open_mfdataset(wave_path)
     nowave_in = xr.open_mfdataset(nowave_path)
 
@@ -37,35 +37,38 @@ def main():
     wave_ds = wave_in["SST"] - 273.15 #deg C
     nowave_ds = nowave_in["SST"] - 273.15 #deg C
 
-    #use groupby() to put data into weekly segments 
-    ostia = ostia_in.convert_calendar(calendar = "standard").groupby("time.week").mean()
-    wave = wave_ds.convert_calendar(calendar = "standard").groupby("time.week").mean()
-    nowave = nowave_ds.convert_calendar(calendar = "standard").groupby("time.week").mean()
+    #align the datasets so that differnces can be performed
+    ostia_ds, wave_ds, nowave_ds = xr.align(ostia_ds, wave_ds, nowave_ds, join = "left")
 
     #calculate differences for comparison plots
-    waves_no = wave - nowave
+    wave_no = wave_ds - nowave_ds
 
-    wave_ostia = wave - ostia
+    wave_ostia = wave_ds - ostia_ds
 
-    nowave_ostia = nowave - ostia
+    nowave_ostia = nowave_ds - ostia_ds
+    
+    print(wave_no)
+    print(wave_ostia)
+    print(nowave_ostia)
+
+    #use slice() to put data into non calendar weekly subsets
+    wnow1 = wave_no.sel(time = slice("2015-11-01", "2015-11-07")).mean()
+    ow1 = wave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean()
+    onow1 = nowave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean()
 
     #set up figure for the plots
     fig, axs = plt.subplots(nrows = 3, ncols = 4, subplot_kw = {"projection": ccrs.PlateCarree()})
 
-    #plot wave vs no wave diffs
+    #plot wave vs no wave differences 
+    #p1 = axs[0][0].contourf(waves_no[1].xh, waves_no[1].yh, waves_no[1], transform = ccrs.PlateCarree())
+    #p2 = axs[0][1].contourf(waves_no[2].xh, waves_no[2].yh, waves_no[3], transform = ccrs.PlateCarree())
+    #p3 = axs[0][2].contourf(waves_no[3].xh, waves_no[3].yh, waves_no[3], transform = ccrs.PlateCarree())
+    #p4 = axs[0][3].contourf(waves_no[4].xh, waves_no[4].yh, waves_no[4], transform = ccrs.PlateCarree())
 
-    print(ostia_in)
-    print(wave_in)
-    print(nowave_in)
-    p1 = axs[0][0].contourf(waves_no[1].xh, waves_no[1].yh, waves_no[1], transform = ccrs.PlateCarree())
-    p2 = axs[0][1].contourf(waves_no[2].xh, waves_no[2].yh, waves_no[3], transform = ccrs.PlateCarree())
-    p3 = axs[0][2].contourf(waves_no[3].xh, waves_no[3].yh, waves_no[3], transform = ccrs.PlateCarree())
-    p4 = axs[0][3].contourf(waves_no[4].xh, waves_no[4].yh, waves_no[4], transform = ccrs.PlateCarree())
-
-    p5 = axs[1][0].contourf(wave_ostia[1].lat, wave_ostia[1].lon, wave_ostia[1], transform = ccrs.PlateCarree())
-    p6 = axs[1][1].contourf(wave_ostia[2].lat, wave_ostia[2].lon, wave_ostia[2], transform = ccrs.PlateCarree())
-    p7 = axs[1][2].contourf(wave_ostia[3].lat, wave_ostia[3].lon, wave_ostia[3], transform = ccrs.PlateCarree())
-    p8 = axs[1][3].contourf(wave_ostia[4].lat, wave_ostia[4].lon, wave_ostia[4], transform = ccrs.PlateCarree())
+    #p5 = axs[1][0].contourf(wave_ostia[1].lat, wave_ostia[1].lon, wave_ostia[1], transform = ccrs.PlateCarree())
+    #p6 = axs[1][1].contourf(wave_ostia[2].lat, wave_ostia[2].lon, wave_ostia[2], transform = ccrs.PlateCarree())
+    #p7 = axs[1][2].contourf(wave_ostia[3].lat, wave_ostia[3].lon, wave_ostia[3], transform = ccrs.PlateCarree())
+    #p8 = axs[1][3].contourf(wave_ostia[4].lat, wave_ostia[4].lon, wave_ostia[4], transform = ccrs.PlateCarree())
 
     plt.savefig("bigcomp_15.png")
 
