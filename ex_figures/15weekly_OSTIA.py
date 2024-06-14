39b304db10404dd746cafaabb7b169133c05ad98
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-14 14:35:49 -0500

    Problems with graphing

--- ex_figures/15weekly_OSTIA.py
+++ ex_figures/15weekly_OSTIA.py
@@ -28,7 +28,7 @@ def main():
     nowave_path = "/work2/noaa/marine/ljones/30day_tests/Nov15_S2SW/COMROOT/Nov15_S2SW/gefs.20151101/00/mem000/products/ocean/netcdf/*.nc"
 
     #read the data in
-    ostia_in = xr.open_mfdataset(ostia_path).rename({"lat": "yh", "lon": "xh"})
+    ostia_in = xr.open_mfdataset(ostia_path).rename({"lat": "yh", "lon": "xh"}).sel(time = slice("2015-11-01", "2015-12-02"))
     wave_in = xr.open_mfdataset(wave_path)
     nowave_in = xr.open_mfdataset(nowave_path)
 
@@ -46,29 +46,40 @@ def main():
     wave_ostia = wave_ds - ostia_ds
 
     nowave_ostia = nowave_ds - ostia_ds
-    
-    print(wave_no)
-    print(wave_ostia)
-    print(nowave_ostia)
 
     #use slice() to put data into non calendar weekly subsets
-    wnow1 = wave_no.sel(time = slice("2015-11-01", "2015-11-07")).mean()
-    ow1 = wave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean()
-    onow1 = nowave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean()
+    wnow1 = wave_no.sel(time = slice("2015-11-01", "2015-11-07")).mean(dim = "time")
+    wnow2 = wave_no.sel(time = slice("2015-11-08", "2015-11-14")).mean(dim = "time")
+    wnow3 = wave_no.sel(time = slice("2015-11-18", "2015-11-24")).mean(dim = "time")
+    wnow4 = wave_no.sel(time = slice("2015-11-25", "2015-12-01")).mean(dim = "time")
+
+    ow1 = wave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean(dim = "time")
+    ow2 = wave_ostia.sel(time = slice("2015-11-08", "2015-11-14")).mean(dim = "time")
+    ow3 = wave_ostia.sel(time = slice("2015-11-18", "2015-11-24")).mean(dim = "time")
+    ow4 = wave_ostia.sel(time = slice("2015-11-25", "2015-12-01")).mean(dim = "time")
+
+    onow1 = nowave_ostia.sel(time = slice("2015-11-01", "2015-11-07")).mean(dim = "time")
+    onow2 = nowave_ostia.sel(time = slice("2015-11-08", "2015-11-14")).mean(dim = "time")
+    onow3 = nowave_ostia.sel(time = slice("2015-11-18", "2015-11-24")).mean(dim = "time")
+    onow4 = nowave_ostia.sel(time = slice("2015-11-25", "2015-12-01")).mean(dim = "time")
+
+    print(wnow3[30][10].values)
 
     #set up figure for the plots
     fig, axs = plt.subplots(nrows = 3, ncols = 4, subplot_kw = {"projection": ccrs.PlateCarree()})
 
     #plot wave vs no wave differences 
-    #p1 = axs[0][0].contourf(waves_no[1].xh, waves_no[1].yh, waves_no[1], transform = ccrs.PlateCarree())
-    #p2 = axs[0][1].contourf(waves_no[2].xh, waves_no[2].yh, waves_no[3], transform = ccrs.PlateCarree())
-    #p3 = axs[0][2].contourf(waves_no[3].xh, waves_no[3].yh, waves_no[3], transform = ccrs.PlateCarree())
-    #p4 = axs[0][3].contourf(waves_no[4].xh, waves_no[4].yh, waves_no[4], transform = ccrs.PlateCarree())
+    p1 = axs[0][0].contourf(wnow1.xh, wnow1.yh, wnow1, transform = ccrs.PlateCarree())
+    p2 = axs[0][1].contourf(wnow2.xh, wnow2.yh, wnow2, transform = ccrs.PlateCarree())
+    p3 = axs[0][2].contourf(wnow3.xh, wnow3.yh, wnow3, transform = ccrs.PlateCarree())
+    p4 = axs[0][3].contourf(wnow4.xh, wnow4.yh, wnow4, transform = ccrs.PlateCarree())
 
-    #p5 = axs[1][0].contourf(wave_ostia[1].lat, wave_ostia[1].lon, wave_ostia[1], transform = ccrs.PlateCarree())
-    #p6 = axs[1][1].contourf(wave_ostia[2].lat, wave_ostia[2].lon, wave_ostia[2], transform = ccrs.PlateCarree())
-    #p7 = axs[1][2].contourf(wave_ostia[3].lat, wave_ostia[3].lon, wave_ostia[3], transform = ccrs.PlateCarree())
-    #p8 = axs[1][3].contourf(wave_ostia[4].lat, wave_ostia[4].lon, wave_ostia[4], transform = ccrs.PlateCarree())
+    p5 = axs[1][0].contourf(ow1.xh, ow1.yh, ow1, transform = ccrs.PlateCarree())
+    p6 = axs[1][1].contourf(ow2.xh, ow2.yh, ow2, transform = ccrs.PlateCarree())
+    p7 = axs[1][2].contourf(ow3.xh, ow3.yh, ow3, transform = ccrs.PlateCarree())
+    p8 = axs[1][3].contourf(ow4.xh, ow4.yh, ow4, transform = ccrs.PlateCarree())
+
+    p9 = axs[2][0].contourf(onow1.xh, onow1.yh, onow1, transform = ccrs.PlateCarree())
 
     plt.savefig("bigcomp_15.png")
 
