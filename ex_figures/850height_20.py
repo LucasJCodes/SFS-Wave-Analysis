411cf938b2f28be1efb1e42ab2a2628bb4d88783
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-06-18 15:43:47 -0500

    Polishing plot visuals

--- ex_figures/850height_20.py
+++ ex_figures/850height_20.py
@@ -52,25 +52,27 @@ def main():
 
     #plot the differences
     fig, axs = plt.subplots(nrows = 2, ncols = 2, subplot_kw = {"projection": ccrs.PlateCarree()})
+    fig.suptitle("Difference in 850hPa Geopotential Height (waves - w/o waves)", fontsize = 12)
+    plt.subplots_adjust(wspace = 0.10, hspace = 0.0001)
 
     ax1 = axs[0][0].contourf(diff1.longitude, diff1.latitude, diff1, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[0][0].coastlines()
-    axs[0][0].set_title("Nov 1-7")
+    axs[0][0].set_title("Nov 1-7", loc = "left", pad = 4.0, fontsize = 10)
 
     ax2 = axs[0][1].contourf(diff2.longitude, diff2.latitude, diff2, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[0][1].coastlines()
-    axs[0][1].set_title("Nov. 8-14")
+    axs[0][1].set_title("Nov. 8-14", loc = "left", pad = 4.0, fontsize = 10)
 
     ax3 = axs[1][0].contourf(diff3.longitude, diff3.latitude, diff3, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[1][0].coastlines()
-    axs[1][0].set_title("Nov. 18-24")
+    axs[1][0].set_title("Nov. 18-24", loc = "left", pad = 4.0, fontsize = 10)
 
     ax4 = axs[1][1].contourf(diff4.longitude, diff4.latitude, diff4, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[1][1].coastlines()
-    axs[1][1].set_title("Nov. 25-Dec 1")
+    axs[1][1].set_title("Nov. 25-Dec 1", loc = "left", pad = 4.0, fontsize = 10)
     
-    cax = fig.add_axes([0.2, 0.08, 0.6, 0.04])
-    plt.colorbar(ax4, cax = cax, extend = "both", orientation = "horizontal")
+    cax = fig.add_axes([0.2, 0.1, 0.6, 0.04])
+    plt.colorbar(ax4, cax = cax, extend = "both", orientation = "horizontal", label = "meters")
 
     plt.savefig("pres850_comp.png")
 
