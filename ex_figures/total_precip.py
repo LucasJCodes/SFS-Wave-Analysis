b3f3bf9af4722949021bbbbbbe618e29e4450ebc
Lucas Jones <ljones@orion-login-2.hpc.msstate.edu>
2024-06-28 10:39:27 -0500

    Latest version for Neil

--- ex_figures/total_precip.py
+++ ex_figures/total_precip.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#SBATCH -J precip_diffs.py
-#SBATCH -o out_precip
-#SBATCH -e error_precip
+#SBATCH -J total_precip.py
+#SBATCH -o out_totalp
+#SBATCH -e error_totalp
 #SBATCH -q debug
 #SBATCH -A marine-cpu
 #SBATCH --ntasks=1
@@ -17,6 +17,10 @@
 ############################
 
 #This program plots the total precipitation from 30 day SFS model runs with IC 2020-11-01 with and without waves for comparison
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import xarray as xr
 
 def main():
     
@@ -33,11 +37,41 @@ def main():
     nowaves = nowaves_in["APCP_surface"] * 1000 / 100**3 * 39.3701 #kg/m^2 to inches using water density 1 g/m^c
     nowaves_in.close()
 
-    #ensure datasets are for same time period
+    #ensure datasets are for same time period and sum data along the time period
+    precip_waves = waves.sel(time = slice("2020-11-01", "2020-11-30")).sum(dim = "time")
+    precip_nowaves = nowaves.sel(time = slice("2020-11-01", "2020-11-30")).sum(dim = "time") 
 
-    #sum data long time
+    #get data max and mins to determine the colorbar range
+    w_max = precip_waves.max().values
+    w_min = precip_waves.max().values
+
+    now_max = precip_waves.max().values
+    now_min = precip_waves.min().values
+
+    if w_max > now_max:
+        vmax = w_max
+    else:
+        vmax = now_max
+
+    if w_min < now_min:
+        vmin = w_min
+    else:
+        vmin = now_min
 
     #plot sums
+    fig, axs = plt.subplots(nrows = 2, ncols = 1, subplot_kw = {"projection": ccrs.PlateCarree()})
+
+    ax1 = axs[0].contourf(precip_waves.longitude, precip_waves.latitude, precip_waves, transformation = ccrs.PlateCarree(), vmax = vmax, vmin = vmin)
+    axs[0].coastlines()
+
+    ax2 = axs[1].contourf(precip_nowaves.longitude, precip_nowaves.latitude, precip_nowaves, transformation = ccrs.PlateCarree(), vmax = vmax, vmin = vmin)
+    axs[1].coastlines()
+
+    fig.colorbar(ax2, location = "bottom", extend = "both") 
+
+    plt.savefig("total_precip.png")
+
+    print("test")
 
 if __name__ == "__main__":
     main()
