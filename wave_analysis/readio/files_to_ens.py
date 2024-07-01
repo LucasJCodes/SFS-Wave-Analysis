c2a93fb55694c0464be2223d9cb924ee96f2a5cf
Lucas Jones <ljones@hercules-login-1.hpc.msstate.edu>
2024-07-01 15:01:15 -0500

    Initial version of function

---
+++ wave_analysis/readio/files_to_ens.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+#SBATCH -J total_precip.py
+#SBATCH -o out_totalp
+#SBATCH -e error_totalp
+#SBATCH -q debug
+#SBATCH -A marine-cpu
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=0
+#SBATCH -t 05:00
+
+############################
+#Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
+#Date: 6/24/24
+############################
+
+#This program reads in an arbitrary number of netcdf datasets at onetime, subsetting them using a another function, and then takes the ensemble mean and returns it as a single, separate file.  
+#Be careful using overlarge datasets.
+
+import ens_mean as em
+import read_sub as rs
+import xarray as xr
+
+def files_to_ens(*filepaths, variable, out_name):
+
+    #for each file in the group of file paths iteratively open it, subtset, and add it to a list of subsetted files. closing the io channel for each dynamically
+
+    data_list = []
+
+    for file in filepaths:
+        data_list.append(rs.read_sub(file, variable))
+    
+    #once files are read in and subsetted, calculate the ensemble mean
+    ens = em.ens_mean(data_list)
+
+    ens.to_netcdf(path = ("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/readio/") + out_name, mode = "w")
+
+if __name__ == "__main__":
+    main()
