9e772073b11b642be34ba7390cb62dbbf4cab864
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-02 13:46:27 -0500

    Fixed bug vmin - vmax

--- wave_analysis/metplot/cont_levels.py
+++ wave_analysis/metplot/cont_levels.py
@@ -8,9 +8,11 @@
 #Function to determine the level/bin divisions given a max and min value and number od desired levels.
 #This can be used to ensure plots have the same contour levels between multiple plots of different data.
 
+import numpy as np
+
 def cont_levels(vmin, vmax, nlevels):
 
-    step = (vmin - vmax) / nlevels
+    step = (vmax - vmin) / nlevels
     levels = np.arange(vmin, vmax + 0.1, step) # levels for the contour p (+0.1 to make upper inclusive)
 
     return levels
