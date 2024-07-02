c7a92d6b1a4bb72903f8fd37c0f3da6f00c7c9b4
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-02 12:02:57 -0500

    Removed unnecessary variables

--- wave_analysis/precip_ensemble.py
+++ wave_analysis/precip_ensemble.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#SBATCH -J precip_weekly.py
-#SBATCH -o precip_weekly_out
-#SBATCH -e precip_weekly_error
+#SBATCH -J precip_ensemble.py
+#SBATCH -o precip_ens_out
+#SBATCH -e precip_ens_error
 #SBATCH -q debug
 #SBATCH -A marine-cpu
 #SBATCH --ntasks=1
@@ -22,28 +22,31 @@
 import sys
 sys.path.append("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis")
 
-from readio import files_to_ensemble as fe
+from readio import files_to_ens as fe
 
 def main():
     
     #file paths for each condition
     #waves 1997
     w1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/atmos/grib2/1p00/gefs.t00z.pgrb2.1p00.f*.nc"
+    w1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/atmos/grib2/1p00/gefs.t00z.pgrb2.1p00.f*.nc"
 
     #waves 2015
 
     #waves 2020
 
     #nowaves 1997
+    now1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/atmos/grib2/1p00/gefs.t00z.pgrb2.1p00.f*.nc"
+    now1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/atmos/grib2/1p00/gefs.t00z.pgrb2.1p00.f*.nc"
 
     #nowaves 2015
 
     #nowaves 2020
 
     #call the method to handle ensemble file creation for each grouping
-    w1997_ens = fe.files_to_ens()
+    fe.files_to_ens("APCP_surface", "precip1997w_ensemble.nc", w1997mem0, w1997mem1)
 
-    now1997_ens = fe.files_to_ens()
+    fe.files_to_ens("APCP_surface", "precip1997now_ensemble.nc", now1997mem0, now1997mem1)
 
 if __name__ == "__main__":
     main()
