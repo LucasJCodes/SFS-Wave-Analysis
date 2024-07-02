a71cb687216ff63de438ea736ae0215c909ba40e
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-02 13:25:37 -0500

    Function to create equal and shared contour levels

---
+++ wave_analysis/metplot/cont_levels.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+############################
+#Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
+#Date: 7/2/24
+############################
+
+#Function to determine the level/bin divisions given a max and min value and number od desired levels.
+#This can be used to ensure plots have the same contour levels between multiple plots of different data.
+
+def cont_levels(vmin, vmax, nlevels):
+
+    step = (vmin - vmax) / nlevels
+    levels = np.arange(vmin, vmax + 0.1, step) # levels for the contour p (+0.1 to make upper inclusive)
+
+    return levels
