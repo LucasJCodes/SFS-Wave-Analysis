e55b48f1321a0e8f775d71c385dd113c47bf1c4f
Lucas Jones <ljones@hercules-login-3.hpc.msstate.edu>
2024-07-08 08:50:37 -0500

    Succesfully moves new files to new location

--- g2nc.sh
+++ g2nc.sh
@@ -12,12 +12,17 @@
 
 #Script to convert grib2 files to netcdf
 files='/work/noaa/marine/nbarton/RUNS/COMROOT/WAVETEST_1997110100_S2SW/gefs.19971101/00/mem000/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2'
+ndir='/work/noaa/marine/nbarton/RUNS/COMROOT/WAVETEST_1997110100_S2SW/gefs.19971101/00/mem000/products/ocean/grib2/1p00/'
 out_dir='/work2/noaa/marine/ljones/90day_experiments/waves/WAVETEST_1997110100_S2SW/gefs.19971101/00/mem000/products/ocean/grib2/1p00'
 
+cd ${ndir}
+files=`ls gefs.ocean.t00z.1p00.f*.grib2`
+
 mkdir -p ${out_dir}
+cd $out_dir
 
 for file in $files
 do
-        wgrib2 ${file} -netcdf ${out_dir}/${file}.nc
-	echo ${file} -netcdf ${out_dir}/${file}.nc
+        wgrib2 ${ndir}/${file} -netcdf ${file}.nc
+	echo ${ndir} -netcdf ${file}.nc
 done
