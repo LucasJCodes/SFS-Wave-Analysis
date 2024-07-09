f657ac81f6bde5aaa55996317dfd1ed644d6ccfd
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-09 10:09:04 -0500

    Changed program for greater efficiency

--- wave_analysis/SST_ensemble.py
+++ wave_analysis/SST_ensemble.py
@@ -9,15 +9,16 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=0
-#SBATCH -t 05:00
+#SBATCH -t 30:00
 
 ############################
 #Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
 #Date: 6/24/24
 ############################
 
-#This program calls functions to create 6 files  with the ensemble mean of model outputted SSTs for 
-#each of the 6 conditions respectively (with and without waves, for years 1997, 2015, 2020)
+#This program calls functions to create a file  with the ensemble mean of model outputted SSTs for 
+#different initial conditions respectively (with and without waves, for years 1997, 2015, 2020).
+#Data file paths for each intial condition are manually loaded.
 
 import sys
 sys.path.append("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis")
@@ -26,26 +27,23 @@ from readio import files_to_ens as fe
 
 def main():
     
+    OUT_FILE = "SST1997now_ensemble.nc"
+
     #The filepaths for each model output data
-        #waves 1997
-    w1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2.nc" 
-    w1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2.nc"
+    updir = "/work2/noaa/marine/ljones/90day_experiments/no_waves/WAVETEST_1997110100_S2S/gefs.19971101/00/"
+    sub = "/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2.nc"
 
-        #waves 2015
+    filelist = []
 
-        #waves 2020
+    for i in range(0, 11):
+        if i >= 10:
+            filelist.append(updir + "mem0" + str(i) + sub)
 
-        #no waves 1997
-    now1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2.nc"
-    now1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/ocean/grib2/1p00/gefs.ocean.t00z.1p00.f*.grib2.nc"
-        #no waves 2015
-
-        #no waves 2020
+        else:
+            filelist.append(updir + "mem00" + str(i) + sub)
 
     #call the method to handle ensemble file creation for each grouping
-    fe.files_to_ens("WTMP_surface", "SST1997w_ensemble.nc", w1997mem0, w1997mem1)
-
-    fe.files_to_ens("WTMP_surface", "SST1997now_ensemble.nc", now1997mem0, now1997mem1)
+    fe.files_to_ens("WTMP_surface", OUT_FILE, filelist)
 
 if __name__ == "__main__":
     main()
