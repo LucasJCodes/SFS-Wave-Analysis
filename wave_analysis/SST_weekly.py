e6c7496dcd5e1e56b22a56cadb88bb56bc81a3c3
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-09 15:52:10 -0500

    Improved generalizability

--- wave_analysis/SST_weekly.py
+++ wave_analysis/SST_weekly.py
@@ -29,9 +29,11 @@ import xarray as xr
 
 def main():
 
+    YEAR = "1997"
+
     #the filepath for the ensemble mean SST data
-    waves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/SST1997w_ensemble.nc" 
-    nowaves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/SST1997now_ensemble.nc" 
+    waves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/SST" + YEAR + "w_ensemble.nc" 
+    nowaves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/SST" + YEAR + "now_ensemble.nc" 
 
     #read in data
     waves_in = xr.open_mfdataset(waves)["WTMP_surface"] - 273.15  #convert to deg C
@@ -44,10 +46,10 @@ def main():
     diff = waves_in - nowaves_in
 
     #subset into weekly periods (the first two and last two weeks of the period) and calculate the mean for each week (and make datarray for graphing but selecing var)
-    week1 = diff.sel(time = slice("1997-11-01", "1997-11-07")).mean(dim = "time")
-    week2 = diff.sel(time = slice("1997-11-08", "1997-11-14")).mean(dim = "time")
-    week3 = diff.sel(time = slice("1998-01-16", "1998-01-22")).mean(dim = "time")
-    week4 = diff.sel(time = slice("1998-01-23", "1998-01-29")).mean(dim = "time")
+    week1 = diff.sel(time = slice(YEAR + "-11-01", YEAR + "-11-07")).mean(dim = "time")
+    week2 = diff.sel(time = slice(YEAR + "-11-08", YEAR + "-11-14")).mean(dim = "time")
+    week3 = diff.sel(time = slice(YEAR + "-11-15", YEAR + "-11-21")).mean(dim = "time")
+    week4 = diff.sel(time = slice(YEAR + "-11-22", YEAR + "-11-28")).mean(dim = "time")
 
     print(week1)
     print(week2)
@@ -56,6 +58,8 @@ def main():
 
     #find overall data min and max for consistent contours and single colorbar
     vmin, vmax = data_range.data_range(diff)
+    
+    print("vmin: " + str(vmin) + " vmax: " + str(vmax))
 
     levs = cont_levels.cont_levels(vmin, vmax, 10)
 
@@ -64,25 +68,25 @@ def main():
     
     ax1 = axs[0][0].contourf(week1.longitude, week1.latitude, week1, levels = levs, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[0][0].coastlines()
-    axs[0][0].set_title("Nov. 1-7, 1997", loc = "left")
+    axs[0][0].set_title("Nov. 1-7, " + YEAR, loc = "left")
 
     ax2 = axs[0][1].contourf(week2.longitude, week2.latitude, week2, levels = levs, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[0][1].coastlines()
-    axs[0][1].set_title("Nov. 8-14, 1997", loc = "left")
+    axs[0][1].set_title("Nov. 8-14, " + YEAR, loc = "left")
 
     ax3 = axs[1][0].contourf(week3.longitude, week3.latitude, week3, levels = levs, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
     axs[1][0].coastlines()
-    axs[1][0].set_title("Jan. 16-22, 1998", loc = "left")
+    axs[1][0].set_title("Nov. 15-21, " + YEAR, loc = "left")
 
     ax4 = axs[1][1].contourf(week4.longitude, week4.latitude, week4, levels = levs, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic", extend = "both")
     axs[1][1].coastlines()
-    axs[1][1].set_title("Jan. 23-29, 1998", loc = "left")
+    axs[1][1].set_title("Nov. 22-18, " + YEAR, loc = "left")
     
     plt.colorbar(ax4, ax = axs, location = "bottom", label = "deg C", extend = "both", pad = 0.05)
 
     fig.suptitle("Difference in Sea Surface Temperatures")
 
-    plt.savefig("SST_weekly.png")
+    plt.savefig("SST" + YEAR + "weekly.png")
 
 if __name__ == "__main__":
     main()
