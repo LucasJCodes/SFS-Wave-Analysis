c740638bdb194935f2a7f831cd5eadfa44265f0d
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-10 09:39:17 -0500

    Changed data parsing and colorbar map to plot data

--- wave_analysis/mldepth_weekly.py
+++ wave_analysis/mldepth_weekly.py
@@ -9,7 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=0
-#SBATCH -t 05:00
+#SBATCH -t 10:00
 
 ############################
 #Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
@@ -29,51 +29,55 @@ import xarray as xr
 
 def main():
 
+    YEAR = "1997"
+
     #paths for the ensemble values
-    path_waves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/MLD1997w_ensemble.nc"
-    path_nowaves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/MLD1997now_ensemble.nc"
+    path_waves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/MLD" + YEAR + "w_ensemble.nc"
+    path_nowaves = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/MLD" + YEAR + "now_ensemble.nc"
 
     #read in the data
-    waves_in = xr.open_mfdataset(path_waves)
-    #waves = waves_in.sel(time = (waves_in.time.dt.hour == 12))
+    waves_in = xr.open_mfdataset(path_waves)["WDEPTH_mixedlayerdepth"]
 
-    nowaves_in = xr.open_mfdataset(path_nowaves)
+    nowaves_in = xr.open_mfdataset(path_nowaves)["WDEPTH_mixedlayerdepth"]
 
     print(waves_in)
-    print(waves_in)
+    print(nowaves_in)
 
     #calculate the difference
     diff = waves_in - nowaves_in
 
     #break into weekly segments
-    week1 = diff.sel(time = slice("1997-11-01", "1997-11-07"))
-    week2 = diff.sel(time = slice("1997-11-08", "1997-11-14"))
-    week3 = diff.sel(time = slice("1997-11-15", "1997-11-21"))
-    week4 = diff.sel(time = slice("1997-11-22", "1997-11-28"))
+    week1 = diff.sel(time = slice(YEAR + "-11-01", YEAR + "-11-07")).mean(dim = "time")
+    week2 = diff.sel(time = slice(YEAR + "-11-08", YEAR + "-11-14")).mean(dim = "time")
+    week3 = diff.sel(time = slice(YEAR + "-11-15", YEAR + "-11-21")).mean(dim = "time")
+    week4 = diff.sel(time = slice(YEAR + "-11-22", YEAR + "-11-28")).mean(dim = "time")
 
     #find overall data min and max for consistent contours and single colorbar
-    vmin, vmax = dr.data_range(diff)
+    vmin = 0
+    vmax = diff.max().values
 
-    levels = cl.cont_levels(vmin, vmax, 10)
+    levels = cl.cont_levels(vmin, vmax, 15)
 
     #plot the differences
     fig, axs = plt.subplots(nrows = 2, ncols = 2, subplot_kw = {"projection": ccrs.PlateCarree()})
 
-    axs[0][0].contourf(week1.longitude, week1.latitude, week1, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[0][0].contourf(week1.longitude, week1.latitude, week1, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree())
     axs[0][0].coastlines()
-    axs[0][0].set_title("Nov. 1-14, 1997", loc = "left")
+    axs[0][0].set_title("Nov. 1-14, " + YEAR, loc = "left")
     
-    axs[0][1].contourf(week2.longitude, week2.latitude, week2, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[0][1].contourf(week2.longitude, week2.latitude, week2, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree())
     axs[0][1].coastlines()
-    axs[0][1].set_title("Nov. 7-14, 1997", loc = "left")
+    axs[0][1].set_title("Nov. 7-14, " + YEAR, loc = "left")
 
-    axs[1][0].contourf(week3.longitude, week3.latitude, week3, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[1][0].contourf(week3.longitude, week3.latitude, week3, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree())
     axs[1][0].coastlines()
-    axs[1][0].set_title("Nov. 15-21, 1997", loc = "left")
+    axs[1][0].set_title("Nov. 15-21, " + YEAR, loc = "left")
 
-    axs[1][1].contourf(week4.longitude, week4.latitude, week4, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[1][1].contourf(week4.longitude, week4.latitude, week4, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree())
     axs[1][1].coastlines()
-    axs[1][1].set_title("Nov. 22-28, 1997", loc = "left")
+    axs[1][1].set_title("Nov. 22-28, " + YEAR, loc = "left")
+
+    plt.savefig("mldepth" + YEAR + "weekly.png")
 
 if __name__ == "__main__":
     main()
