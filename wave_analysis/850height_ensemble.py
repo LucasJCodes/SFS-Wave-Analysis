710468de6412a2b7277efc804d7fcf2ad33ebee6
Lucas Jones <ljones@hercules-login-2.hpc.msstate.edu>
2024-07-11 16:05:40 -0500

    Major change of way ensemble is calculated

--- wave_analysis/850height_ensemble.py
+++ wave_analysis/850height_ensemble.py
@@ -3,20 +3,19 @@
 #SBATCH -J 850height_ensemble.py
 #SBATCH -o 850height_ens_out
 #SBATCH -e 850height_ens_error
-#SBATCH -q debug
 #SBATCH -A marine-cpu
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=0
-#SBATCH -t 05:00
+#SBATCH -t 20:00
 
 ############################
 #Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
 #Date: 7/2/24
 ############################
 
-#This program calls functions to create a 6 files wth the ensemble mean of model outputted 850 hPa geopotential heights for
+#This program calls functions to create a file with the ensemble mean of model outputted 850 hPa geopotential heights for
 #each of the 6 respective conditions(with and without waves, for years 1997, 2015, 2020)
 
 import sys
@@ -26,27 +25,24 @@ from readio import files_to_ens as fe
 
 def main():
 
-    #file paths for each condition
-    #waves 1997
-    w1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f*" 
-    w1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f*"
+    OUT_FILE = "850Height1997w_ensemble.nc"
 
-    #waves 2015
+    #The filepaths for each model output data
+    updir = "/work2/noaa/marine/ljones/90day_experiments/waves/WAVETEST_1997110100_S2SW/gefs.19971101/00/"
+    sub = "/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f*.nc"
 
-    #waves 2020
+    filelist = []
 
-    #nowaves 1997
-    now1997mem0 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem001/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f*"
-    now1997mem1 = "/work2/noaa/marine/ljones/90day_experiments/no_waves/IC1997/COMROOT/IC1997/gefs.19971101/00/mem002/products/atmos/grib2/1p00/gefs.t00z.pgrb2b.1p00.f*"
+    for i in range(0, 11):
+        if i >= 10:
+            filelist.append(updir + "mem0" + str(i) + sub)
 
-    #nowaves 2015
-
-    #nowaves 2020
+        else:
+            filelist.append(updir + "mem00" + str(i) + sub)
 
     #call the method to handle ensemble file creation for each grouping
-    fe.files_to_ens("HGT_850mb", "850heigt1997w_ensemble.nc", w1997mem0, w1997mem1)
+    fe.files_to_ens("HGT_850mb", OUT_FILE, filelist)
 
-    fe.files_to_ens("HGT_850mb", "850height1997now_ensemble.nc", now1997mem0, now1997mem1)
 
 if __name__ == "__main__":
     main()
