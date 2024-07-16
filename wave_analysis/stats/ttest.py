12c9587548f4aac28506cb0287a38a6ec0c50814
Lucas Jones <ljones@hercules-login-2.hpc.msstate.edu>
2024-07-16 10:47:31 -0500

    Fixed data handling bugs

--- wave_analysis/stats/ttest.py
+++ wave_analysis/stats/ttest.py
@@ -9,11 +9,21 @@
 #gridded pvalues for locations that exceed the significance threshold, alpha.
 #The t test is two sidded, performed along the first axis, and assumes equal variance
 
-def ttest(data1, data2, alpha)
+from cartopy.util import add_cyclic_point
+import numpy as np
+from scipy import stats
+import xarray as xr
+
+def ttest(data1, data2, alpha):
+
+    #print("variance 1: ")
+    #print(data1.var())
+    #print("variance 2: ")
+    #print(data2.var())
 
     #perform the t test
     tstat, pval = stats.ttest_ind(data1, data2, axis = 0, equal_var = True, alternative = "two-sided")
-
+    
     #create latitude and longitude arrays for the plotting the p values
     lat = np.arange(-90, len(pval[:, 0]) - 90)  #-90 to make it -90 to 90 latitude
     lon = np.arange(0, len(pval[0, :]) + 1)  #add 1 to make 0 to 361 for adding cyclical point later
@@ -22,6 +32,6 @@ def ttest(data1, data2, alpha)
     xpval = xr.DataArray(add_cyclic_point(pval), coords = {"latitude": lat, "longitude": lon}, dims = ["latitude", "longitude"])
 
     #iget only statistically significant p values for plotting
-    sig_pval = xpval.where(xpval.values < 0.05)
+    sig_pval = xpval.where(xpval.values <= alpha)
 
     return sig_pval
