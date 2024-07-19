c2cc8d185ec65d6a504a3fb083f1c6d2fff56d29
Lucas Jones <ljones@hercules-login-4.hpc.msstate.edu>
2024-07-19 10:24:03 -0500

    Wrapper function for handeling pvals and performing t test

---
+++ wave_analysis/stats/monthly_ttest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+############################
+#Author: Lucas Jones, Hollings Scholar Intern at NOAA EMC
+#Date: 7/19/24
+############################
+
+#This is a simple wrapper/helper function to read in data for a monthly period  and then calculate p values using the ttest()
+#function.  These p values are returned as xarray datasets for graphing.
+
+import sys
+sys.path.append("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/readio")
+
+from stats import ttest
+import xarray as xr
+
+def weekly_ttest(variable, year, start_month, alpha):
+
+    waves_in = xr.open_dataset("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/monthly/" + variable + "w" + year + start_month + ".nc")
+    nowaves_in = xr.open_dataset("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/monthly/" + variable + year + start_month + ".nc")
+
+    pvals = ttest.ttest(waves_in, nowaves_in, alpha)
+
+    return pvals
