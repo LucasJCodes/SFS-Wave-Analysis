2bb09ef697b2ab7e7fd3f76a7f6066f159e33d65
Lucas Jones <ljones@hercules-login-3.hpc.msstate.edu>
2024-07-25 10:11:21 -0500

    Added t testing and stat significance plotting

--- wave_analysis/850height_weekly.py
+++ wave_analysis/850height_weekly.py
@@ -22,19 +22,21 @@
 import sys
 sys.path.append("/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/")
 
+from stats import weekly_ttest
 from metplot import data_range
 import cartopy.crs as ccrs
+import matplotlib as mpl
 from metplot import cont_levels
 import matplotlib.pyplot as plt
 import xarray as xr
 
 def main():
     
-    YEAR = "1997"
+    YEAR = "2020"
 
     #file path for the ensemble means
-    wave_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/850Height1997w_ensemble.nc" 
-    nowave_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/850Height1997now_ensemble.nc"
+    wave_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/850Height" + YEAR + "w_ensemble.nc" 
+    nowave_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/850Height" + YEAR + "now_ensemble.nc"
 
     #open the data
     nowave_in = xr.open_mfdataset(nowave_path)
@@ -57,30 +59,43 @@ def main():
     week4 = diff.sel(time = slice(YEAR + "-11-22", YEAR + "-11-28")).mean(dim = "time")
 
     #set the range for the data so the contour levels and colorbar are the same
-    vmin, vmax = data_range.data_range(diff)
+    vmin = -120 #data_range.data_range(diff)
+    vmax = 120
 
     levels = cont_levels.cont_levels(vmin, vmax, 15)
+    
+    #perform t testing to plot statistical significance
+    w1_pvals = weekly_ttest.weekly_ttest("850Height", YEAR, "11", "01", 0.05)
+    w2_pvals = weekly_ttest.weekly_ttest("850Height", YEAR, "11", "08", 0.05)
+    w3_pvals = weekly_ttest.weekly_ttest("850Height", YEAR, "11", "15", 0.05)
+    w4_pvals = weekly_ttest.weekly_ttest("850Height", YEAR, "11", "22", 0.05)
+
+    mpl.rcParams["hatch.linewidth"] = 0.5
 
     #plot the differences
     fig, axs = plt.subplots(nrows = 2, ncols = 2, subplot_kw = {"projection": ccrs.PlateCarree()})
-    fig.suptitle("Difference in 850hPa Geopotential Height (waves - no waves)", fontsize = 12)
+    fig.suptitle("Difference in 850hPa Geopotential Height (waves - no waves) " + YEAR, fontsize = 12)
 
     ax1 = axs[0][0].contourf(week1.longitude, week1.latitude, week1, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[0][0].contourf(w1_pvals.longitude, w1_pvals.latitude, w1_pvals, colors = "none", hatches = ["/"*10], transform = ccrs.PlateCarree())
     axs[0][0].coastlines()
     axs[0][0].gridlines(draw_labels = {"left": "y"}, linestyle = "--", linewidth = 0.5)
     axs[0][0].set_title("Nov. 1-7, " + YEAR, loc = "left")
 
     ax2 = axs[0][1].contourf(week2.longitude, week2.latitude, week2, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[0][1].contourf(w2_pvals.longitude, w2_pvals.latitude, w2_pvals, colors = "none", hatches = ["/"*10], transform = ccrs.PlateCarree())
     axs[0][1].coastlines()
     axs[0][1].gridlines(linestyle = "--", linewidth = 0.5)
     axs[0][1].set_title("Nov. 8-14, " + YEAR, loc = "left")
 
     ax3 = axs[1][0].contourf(week3.longitude, week3.latitude, week3, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic")
+    axs[1][0].contourf(w3_pvals.longitude, w3_pvals.latitude, w3_pvals, colors = "none", hatches = ["/"*10], transform = ccrs.PlateCarree())
     axs[1][0].coastlines()
     axs[1][0].gridlines(draw_labels = {"bottom": "x", "left": "y"}, linestyle = "--", linewidth = 0.5)
     axs[1][0].set_title("Nov. 15-21, " + YEAR, loc = "left")
 
     ax4 = axs[1][1].contourf(week4.longitude, week4.latitude, week4, levels = levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), cmap = "seismic", extend = "both")
+    axs[1][1].contourf(w4_pvals.longitude, w4_pvals.latitude, w4_pvals, colors = "none", hatches = ["/"*10], transform = ccrs.PlateCarree())
     axs[1][1].coastlines()
     axs[1][1].gridlines(draw_labels = {"bottom": "x"}, linestyle = "--", linewidth = 0.5)
     axs[1][1].set_title("Nov. 22-28, " + YEAR, loc = "left")
@@ -88,7 +103,6 @@ def main():
     plt.colorbar(ax4, ax = axs, extend = "both", orientation = "horizontal", label = "meters")
 
     plt.savefig("850height" + YEAR + "weekly.png")
-
 
 if __name__ == "__main__":
     main()
