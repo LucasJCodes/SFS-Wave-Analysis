7121ce07ddde22a9e950930d842c762b9a8d12bd
Lucas Jones <ljones@hercules-login-3.hpc.msstate.edu>
2024-07-26 08:58:12 -0500

    Updated colorbars/scheme and improved plotting

--- wave_analysis/precip_total.py
+++ wave_analysis/precip_total.py
@@ -28,10 +28,12 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 def main():
-    
+
+    YEAR = "1997"
+
     #file paths for wave and non wave precip ensemble means
-    waves_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/readio/precip1997w_ensemble.nc"
-    nowaves_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/readio/precip1997now_ensemble.nc"
+    waves_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/precip" + YEAR + "w_ensemble.nc"
+    nowaves_path = "/work2/noaa/marine/ljones/SFS-Wave-Analysis/wave_analysis/ensembles/precip" + YEAR + "now_ensemble.nc"
 
     #open and subset the wave data first
     waves_in = xr.open_mfdataset(waves_path)
@@ -42,6 +44,9 @@ def main():
     nowaves_in = xr.open_mfdataset(nowaves_path)
     nowaves = nowaves_in["APCP_surface"] * 1000 / 100**3 * 39.3701 #kg/m^2 to inches using water density 1 g/m^c
     nowaves_in.close()
+
+    print(waves_in)
+    print(nowaves_in)
 
     #find the sum total precipitation for each data set
     total_waves = waves.sum(dim = "time")
@@ -62,33 +67,36 @@ def main():
     else:
         vmax = nowmax
 
-    precip_levels = cont_levels.cont_levels(0, vmax, 10)
+    precip_levels = cont_levels.cont_levels(0, vmax, 15)
 
     #get a symetric colorbar range for the diff plot
-    dmin, dmax = data_range.data_range(diff)
+    dmin = -6
+    dmax = 6
+
+    diff_levels = cont_levels.cont_levels(dmin, dmax, 15)
 
     #plot the toal precipitation for each and the difference
     fig, axs = plt.subplots(nrows = 3, ncols = 1, subplot_kw = {"projection": ccrs.PlateCarree()})
 
-    p1 = axs[0].contourf(total_waves.longitude, total_waves.latitude, total_waves, levels = precip_levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), extend = "max")
+    p1 = axs[0].contourf(total_waves.longitude, total_waves.latitude, total_waves, levels = precip_levels, vmin = vmin, vmax = vmax, cmap = "YlGnBu", transform = ccrs.PlateCarree(), extend = "max")
     axs[0].coastlines()
     axs[0].set_title("With Waves", loc = "left")
 
-    p2 = axs[1].contourf(total_nowaves.longitude, total_nowaves.latitude, total_nowaves, levels = precip_levels, vmin = vmin, vmax = vmax, transform = ccrs.PlateCarree(), extend = "max")
+    p2 = axs[1].contourf(total_nowaves.longitude, total_nowaves.latitude, total_nowaves, levels = precip_levels, vmin = vmin, vmax = vmax, cmap = "YlGnBu", transform = ccrs.PlateCarree(), extend = "max")
     axs[1].coastlines()
     axs[1].set_title("Without Waves", loc = "left")
 
     fig.colorbar(p1, ax = axs[0:2].ravel().tolist(), extend = "max", label = "inches of rain")
 
-    p3 = axs[2].contourf(diff.longitude, diff.latitude, diff, transform = ccrs.PlateCarree(), cmap = "seismic", extend = "both")
+    p3 = axs[2].contourf(diff.longitude, diff.latitude, diff, levels = diff_levels, vmin = dmin, vmax = dmax, cmap = "Spectral", transform = ccrs.PlateCarree(), extend = "both")
     axs[2].coastlines()
     axs[2].set_title("Waves - No Waves", loc = "left")
 
-    fig.colorbar(p3, ax = axs[2], extend = "both", label = "Difference in inches")
+    fig.colorbar(p3, ax = axs[2], location = "right", extend = "both", label = "Difference in inches")
 
-    fig.suptitle("Total Precipitation Comparison")
+    fig.suptitle("Total Precipitation Comparison " + YEAR)
 
-    plt.savefig("precip_total.png")
+    plt.savefig("precip" + YEAR + "total.png")
 
 if __name__ == "__main__":
     main()
